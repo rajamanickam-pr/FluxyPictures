7673d9043998eeb16f8a867c9ba079ceeaefa503
Rajamanickam Ramasamy <rajamanickam.rp@gmail.com>
2017-07-27 23:44:56 +0530

    Version Control added

---
+++ Fluxy.Core/Helpers/FluxyVersion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxy.Core.Helpers
+{
+    class FluxyVersion
+    {/// <summary>
+        /// Gets or sets the store version
+        /// </summary>
+        public static string CurrentVersion
+        {
+            get
+            {
+                return "1.0";
+            }
+        }
+    }
+}
